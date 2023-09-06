b64290f5dec0646e060477a5671ddf58b9827df9
Ryan Le <ryandungl3@gmail.com>
2023-09-06 14:28:19 -0500

    use avatar error

--- components/User-avatar.tsx
+++ components/User-avatar.tsx
@@ -7,8 +7,8 @@ export const UserAvatar = () => {
     <Avatar className=" h-8 w-8">
       <AvatarImage src={user?.imageUrl} />
       <AvatarFallback>
-        {user?.firstName?.charAt[0]}
-        {user?.lastName?.charAt[0]}
+        {user?.firstName?.charAt(0)}
+        {user?.lastName?.charAt(0)}
       </AvatarFallback>
     </Avatar>
   );
